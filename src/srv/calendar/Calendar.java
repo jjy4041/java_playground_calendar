c82b0c4cc34f6820ed11492cc3c4659c9f625489
jjy4041 <754343@naver.com>
2021-08-05 18:42:44 +0900

    Fix [getMaxDays] !

--- src/srv/calendar/Calendar.java
+++ src/srv/calendar/Calendar.java
@@ -3,18 +3,28 @@ package srv.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	
+	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 	public static void main(String[] args) {
 	
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("원하는 달을 숫자만 입력해주세요!");
-		int month = scanner.nextInt();
-	
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		Calendar cal = new Calendar();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+		
+		for(int i = 0; i < repeat; i++) {
+			System.out.println("원하는 달을 숫자만 입력해주세요!");
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDays(month));
+		}
 		
 		scanner.close();
+	}
+	
+	public int getMaxDays(int month) {
+		return MAX_DAYS[month - 1];
 	}
 
 }
